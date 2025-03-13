ea416f8aacae6225d8089b43106e32d5dd05dd2b
chunguifriki <99367623+chunguifriki@users.noreply.github.com>
2025-03-13 10:35:11 +0100

    added javadoc to class Debug.kt

--- app/src/main/java/com/example/prova_mb_1/Debug.kt
+++ app/src/main/java/com/example/prova_mb_1/Debug.kt
@@ -6,7 +6,13 @@ import android.util.Log
 import android.widget.Toast
 
 object Debug {
-    val debug = false
+    val debug = false // Set to true to enable debug mode
+    /**
+     * Displays a message as a Toast and logs an error.
+     *
+     * @param context The application context. Must be an instance of Activity to show a Toast.
+     * @param mensaje The message to be displayed and logged.
+     */
     fun showMessage(context: Context, mensaje: String) {
         (context as? Activity)?.runOnUiThread {
             if (debug) {
@@ -15,6 +21,12 @@ object Debug {
             Log.e(context.toString(), mensaje)
         } ?: Log.e("showMessage", "The Toast can not be shown: context is not an instance of Activity")
     }
+    /**
+     * Enables debug mode and displays a toast message if debug mode is active.
+     * Also logs an error message indicating that debug mode has been enabled.
+     *
+     * @param context The application context. Must be an instance of Activity to show a Toast.
+     */
     fun enabled(context: Context) {
         (context as? Activity)?.runOnUiThread {
             if (debug) {
